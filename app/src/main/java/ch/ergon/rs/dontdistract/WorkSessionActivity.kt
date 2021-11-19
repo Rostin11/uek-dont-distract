60323688bdd3f6aaf4269d472691e0ccd3a2f7f1
Rostin11 <57532643+Rostin11@users.noreply.github.com>
2021-11-19 12:55:55 +0100

    Add vibration when Timer is over.

--- app/src/main/java/ch/ergon/rs/dontdistract/WorkSessionActivity.kt
+++ app/src/main/java/ch/ergon/rs/dontdistract/WorkSessionActivity.kt
@@ -6,9 +6,6 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.hardware.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.CountDownTimer
-import android.os.IBinder
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -18,12 +15,17 @@ import kotlin.math.acos
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
+import android.os.*
+import android.view.WindowManager
+import java.lang.StringBuilder
+
+
 class WorkSessionActivity : AppCompatActivity() {
-    lateinit var countDownText: TextView;
-    lateinit var sessionNameField: TextView;
-    lateinit var sessionDateField: TextView;
-    lateinit var countDownButton: Button;
-    lateinit var deleteButton: Button;
+    lateinit var countDownText: TextView
+    lateinit var sessionNameField: TextView
+    lateinit var sessionDateField: TextView
+    lateinit var countDownButton: Button
+    lateinit var deleteButton: Button
     var listIndex: Int = -1
 
     lateinit var countDownTimer: CountDownTimer
@@ -63,6 +65,7 @@ class WorkSessionActivity : AppCompatActivity() {
             } else {
                 // device is not flat
                 if (timerRunning) {
+                    vibrate(700)
                     stopTimer()
                 }
                 hideStartButton()
@@ -79,14 +82,12 @@ class WorkSessionActivity : AppCompatActivity() {
             val binder = service as HandleSessionsService.LocalBinder
             mService = binder.getService()
             mBound = true
-            deleteButton.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    timerRunning = false
-                    mService.deleteSessionByListIndex(listIndex)
-                    var intent = Intent(this@WorkSessionActivity, MainActivity::class.java)
-                    startActivity(intent)
-                }
-            })
+            deleteButton.setOnClickListener {
+                timerRunning = false
+                mService.deleteSessionByListIndex(listIndex)
+                var intent = Intent(this@WorkSessionActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
         }
 
         override fun onServiceDisconnected(arg0: ComponentName) {
@@ -119,14 +120,13 @@ class WorkSessionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_work_session)
+        milliSecondsLeft = intent.extras?.get(MainActivity.REMAINING_SECONDS).toString().toInt()
 
         listIndex = intent.extras?.get(MainActivity.LIST_INDEX).toString().toInt()
         val sessionName = intent.extras?.get(MainActivity.NAME).toString()
-        val remainingMilliSeconds =
-            intent.extras?.get(MainActivity.REMAINING_SECONDS).toString().toInt()
         val endDate = intent.extras?.get(MainActivity.END_DATE) as SessionDate
 
-        milliSecondsLeft = remainingMilliSeconds
+
 
         countDownText = findViewById(R.id.countDownTextView)
         sessionNameField = findViewById(R.id.sessionNameField)
@@ -135,12 +135,13 @@ class WorkSessionActivity : AppCompatActivity() {
         countDownButton = findViewById(R.id.countDownButton)
         deleteButton = findViewById(R.id.deleteButton)
 
+        hideStartButton()
+
+
         sessionNameField.text = sessionName
         sessionDateField.text = "Endet am ${endDate.day}/${endDate.month}/${endDate.year}"
 
         updateTimer()
-
-        if (milliSecondsLeft == 0) hideStartButton()
 
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         mSensorAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
@@ -156,6 +157,7 @@ class WorkSessionActivity : AppCompatActivity() {
     }
 
     fun startTimer() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         countDownTimer = object : CountDownTimer((milliSecondsLeft).toLong(), 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 milliSecondsLeft = millisUntilFinished.toInt()
@@ -163,11 +165,11 @@ class WorkSessionActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                println("finish")
                 milliSecondsLeft = 0
                 stopTimer()
                 updateTimer()
                 hideStartButton()
+                playAlarm()
             }
         }.start()
         countDownButton.setText("PAUSE")
@@ -175,6 +177,7 @@ class WorkSessionActivity : AppCompatActivity() {
     }
 
     fun stopTimer() {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         countDownTimer.cancel()
         countDownButton.setText("START")
         timerRunning = false
@@ -182,26 +185,33 @@ class WorkSessionActivity : AppCompatActivity() {
     }
 
     fun updateTimer() {
-        val millisecondsToShow: Int
+        val millisecondsToShow: Int = milliSecondsLeft
 
-        if (milliSecondsLeft % 60000 == 0) {
-            millisecondsToShow = milliSecondsLeft
-        } else {
-            millisecondsToShow = milliSecondsLeft + 60000
-        }
-        milliSecondsLeft
+        //if (milliSecondsLeft % 60000 == 0) {
+        //    millisecondsToShow = milliSecondsLeft
+        //} else {
+        //    millisecondsToShow = milliSecondsLeft + 60000
+        //}
         val hours: Int = millisecondsToShow / 3600000
-        val minutes: Int = (millisecondsToShow % 3600000) / 60000
+        val minutes: Int = millisecondsToShow % 3600000 / 60000
+        val seconds: Int = millisecondsToShow % 3600000 % 60000 / 1000
 
-        val timeLeftText: String
+        var timeStringBuilder = StringBuilder()
+
+        timeStringBuilder.append("$hours:")
 
         if (minutes < 10) {
-            timeLeftText = "$hours:0$minutes"
+            timeStringBuilder.append("0$minutes:")
         } else {
-            timeLeftText = "$hours:$minutes"
+            timeStringBuilder.append("$minutes:")
+        }
+        if (seconds < 10) {
+            timeStringBuilder.append("0$seconds")
+        } else {
+            timeStringBuilder.append("$seconds")
         }
 
-        countDownText.setText(timeLeftText)
+        countDownText.setText(timeStringBuilder.toString())
 
     }
 
@@ -219,9 +229,26 @@ class WorkSessionActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
+        mSensorManager?.unregisterListener(mSensorEventListener)
+    }
+
+    fun playAlarm(){
+        vibrate(5000)
+    }
+
+    private fun vibrate(length: Int){
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(length.toLong(), 255))
+        } else {
+            vibrator.vibrate(5000)
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
         if (timerRunning) {
             stopTimer()
         }
-        mSensorManager?.unregisterListener(mSensorEventListener)
     }
 }
